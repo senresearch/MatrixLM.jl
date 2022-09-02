0154462ba528e9895b9119dbbc1aeca22ce918cd
GregFa <36859310+GregFa@users.noreply.github.com>
2022-09-02 16:02:45 -0500

    Update MatrixLM.jl

--- src/MatrixLM.jl
+++ src/MatrixLM.jl
@@ -18,7 +18,7 @@ export Response, Predictors, RawData, get_X, get_Z, get_Y, contr,
     kron_diag, add_intercept, remove_intercept, shuffle_rows, shuffle_cols, 
     Mlm, mlm, t_stat, coef, predict, fitted, resid, 
     perm_pvals, mlm_perms, design_matrix, @mlmFormula, is_full_rank, check_Z_rank,
-    Mlm_plots
+    MLMplots
 
 
 # Data object types
