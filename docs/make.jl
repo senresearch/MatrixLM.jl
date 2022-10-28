e5e36ab3fa12ba044470fd417b50846607fe6b57
Gregory Farage <gfarage@uthsc.edu>
2022-10-28 12:06:03 -0500

    renamed Demo1_Simulation.md into getting_started.md; updated docs make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,17 +4,23 @@ using MatrixLM
 const src = "https://github.com/senresearch/MatrixLM.jl"
 
 makedocs(
-         sitename = "MatrixLM",
-         format = Documenter.HTML(),
-         modules  = [MatrixLM],
-         pages=[
-                "Home" => "index.md",
-                "Getting Started" => "Demo1_Simulation.md",
-                "More examples" => "moreExamples.md",
-                "Types and Functions" => "functions.md"
-               ])
+        modules = [MatrixLM],
+        repo="https://github.com/senresearch/MatrixLM.jl/blob/{commit}{path}#{line}",
+        sitename = "MatrixLM.jl",
+        format=Documenter.HTML(
+            prettyurls = get(ENV, "CI", "false") == "true",
+            canonical = "https://senresearch.github.io/MatrixLM.jl",
+        ),       
+        pages=[
+            "Home" => "index.md",
+            "Getting Started" => "getting_started.md",
+            "More examples" => "moreExamples.md",
+            "Types and Functions" => "functions.md"
+        ],
+)
 deploydocs(;
     repo= "https://github.com/senresearch/MatrixLM.jl",
     devbranch= "testing",
+    push_preview = true,
     devurl = "stable"
 )
