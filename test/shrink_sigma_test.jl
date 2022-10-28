41268e593875f53c94aedbb0471f55acb729e97d
Gregory Farage <gfarage@uthsc.edu>
2022-10-28 17:02:21 -0500

    changed doc badge link stable to dev

--- test/shrink_sigma_test.jl
+++ test/shrink_sigma_test.jl
@@ -37,13 +37,13 @@ lambdaD = MatrixLM.shrink_sigma(X, "D")[2]
 
 @testset "shrinkSigmaTesting" begin
     @test isapprox(lambda, lambda2, atol=tol)
-    # compaire the center function
+    # compare the center function
     @test isapprox(mean(MatrixLM.center(X)), 0, atol=tol)
-    # compaire the result of cov_est with cov function
+    # compare the result of cov_est with cov function
     @test isapprox(est, cov(X), atol=tol)
     # test the size of variance matrices
     @test size(varEst) == (p,p)
-    # Test 1. compaire shrink_sigma function
+    # Test 1. compare shrink_sigma function
     @test isapprox(lambdaB, lambdaC, atol=0.1)
     @test isapprox(lambdaB, lambdaD, atol=0.1)
     @test isapprox(lambdaC, lambdaD, atol= 0.1)
