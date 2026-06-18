70db093129deb3f2a5aeef39d4a7b7c25743a3e2
Saunak Sen <sen@uthsc.edu>
2026-06-17 22:19:41 -0500

    Add new shrink_var function; separates correlation and variance shrinksge.

--- src/shrink_sigma.jl
+++ src/shrink_sigma.jl
@@ -146,3 +146,172 @@ function shrink_sigma(resid::AbstractArray{Float64,2}, targetType::String)
         
     return lambda*T + (1-lambda)*est, lambda
 end
+
+# ----------------------------------------------------------------------
+
+"""
+    tri2vec(X::Matrix{Float64}
+
+Extracts the diagonal and the upper triangular elements of a square
+matrix into two vectors.
+
+# Value
+
+A tuple of two vectors with the off diagonal and diagonal
+respectively.
+
+# See also
+
+`tri2vec`
+"""
+function tri2vec(X::Matrix{Float64})
+    m = size(X,1)
+    if(size(X,2)!=m)
+        error("Matrix not square.")
+    end
+    x = zeros(div(m*(m-1),2))
+    d = zeros(m)
+    
+    idx = 1
+    for i in 1:(m-1)
+        for j in ((i+1):m)
+            x[idx] = X[i,j]
+            idx+=1
+        end
+    end
+
+    for i in 1:m
+        d[i] = X[i,i]
+    end
+    return (x,d)
+end
+
+"""
+    vec2tri(X::Matrix{Float64}
+
+Reconstructs a square matrix from two vectors which are the upper
+triangular elements and the diagonal respectively.
+
+# Value
+
+Square symmetric matrix.
+
+# See also
+
+`vec2tri`
+"""
+function vec2tri(x::Vector{Float64},d::Vector{Float64})
+    m = (isqrt(8*length(x)+1)+1) ÷ 2
+    X = zeros(m,m)
+
+    if( m!=length(d) )
+        error("Incompatible dimensions of the two vectors.")
+    end
+    
+    idx = 1
+    for i in 1:(m-1)
+        for j in (i+1):m
+            X[i,j] = X[j,i] =  x[idx]
+            idx+=1
+        end
+    end
+
+    for i in 1:m
+        X[i,i] = d[i]
+    end
+    
+    return X
+end
+
+"""
+    shrink(x::Vector{Float64},t::Float64=0.0,v::Float64=1.0)
+
+Calculates shrinkage estimates based on estimates and their variances
+towards a target.
+
+# Arguments
+
+- `x::Vector{Float64}`: Vector of estimates
+- `t::Float64=0`: Target to shrink to
+- `v::Float64=1.0`: Common variance of the estimates
+
+# Value
+
+Tuple:
+- `x::Vector{Float64}`: Shrunk estimates
+- `λ:Float64`: Shrinkage coefficient (0=no shrinkage; 1=complete shrinkage) 
+
+"""
+function shrink(x::Vector{Float64},t::Float64=0.0,v::Float64=1.0)
+    k = length(x)
+    λ = k*v / sum((x .- t).^2)
+    λ = minimum((1.0,λ))
+    return λ*t .+ (1-λ) .* x, λ
+end
+
+"""
+    shrink_var(resid::Matrix{Float64})
+
+Estimates covariance matrix of residuals using a shrinkage estimator
+
+# Arguments
+
+- `resid::Matrix{Flaot64}`: 2d array of floats consisting of the residuals
+
+# Value
+
+Tuple
+- `sigma`: 2d array of floats; shrunk estimated variance of errors
+- `lambda`: floating scalar; estimated shrinkage coefficient 
+  (0 = no shrinkage, 1 = complete shrinkage)
+
+# Details
+
+The covariance matrix is decomposed into a correlation matrix and a variance
+vector.  They are each transformed by a variance stabilizing transform and
+shrunk towards a common value. Then they are transformed back and the
+covariance matrix is reconstructed.
+
+# Reference
+
+Ledoit, O., & Wolf, M. (2003). Improved estimation of the covariance matrix 
+    of stock returns with an application to portfolio selection. Journal of 
+    empirical finance, 10(5), 603-621.
+
+"""
+function shrink_var(X::Matrix{Float64})
+
+    (n,m) = size(X) # number of samples
+    if (n<=3) 
+        error("Sample size too small for proceeding further.")
+    end
+
+    # variance of correlation and variance after variance stabilizing transform
+    vr = 1/(n-3)
+    vv = 2/(n-2)
+
+    
+    (r,a) = tri2vec(cor(X))
+    r = atanh.(r)
+    v = log.(vec(var(X,dims=1)))
+
+    (λr,r) = shrink(r,mean(r),vr)
+    (λv,v) = shrink(v,mean(v),vv)
+
+    r[:] = tanh.(r)
+    v[:] = exp.(v.+0.5.*(var(v).+mean(vv)))
+    s = sqrt.(v)
+
+    R = vec2tri(r,a)
+    
+    S = Diagonal(s) * R * Diagonal(s)
+    
+end
+
+# checks to perform
+
+# - tri2vec and vec2tri should be inverses of each other
+# - tri2vec should only work for square matrices
+# - vec2tri should have compatible argument lengths
+# - shrunk covariance should be positive definite even if input is not
+
